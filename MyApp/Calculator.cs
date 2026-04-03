d5a0fd9a915f5db604bd41e6a7defec0105ef9ed
sby-y-jige <y-jige@shibuya.co.jp>
2026-04-03 13:36:06 +0900

    プロジェクト ファイルを追加します。

---
+++ MyApp/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+}
+
